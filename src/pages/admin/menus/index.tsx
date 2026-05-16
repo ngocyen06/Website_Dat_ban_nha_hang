c7b75eb13e9f978528dd1b0b3bbbb9b36dbcc85c
Yen <buingocyenphuan@gmail.com>
2026-05-16 07:18:55 +0700

    Update index.tsx

--- src/pages/admin/menus/index.tsx
+++ src/pages/admin/menus/index.tsx
@@ -299,7 +299,7 @@ const AdminMenusPage: NextPage = () => {
                                                 >
                                                     {menu.isAvailable
                                                         ? "Có sẵn"
-                                                        : "Không có sẵn"
+                                                        : "Không có sẵn"}
                                                 </span>
                                             </td>
                                             <td className="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
